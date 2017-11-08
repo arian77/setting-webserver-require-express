ed2a20485a2cd1d2d202e71edd13cde26185b104
Arian <arianmarkus77@yahoo.com>
2017-11-08 22:10:16 +0700

    Create server.js to backend

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// create express app
+var app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a simple route
+app.get("/", function(req, res) {
+  res.json({
+    message: "Here express and nodemon !! ."
+  });
+});
+
+// listen for requests
+app.listen(3000, function() {
+  console.log("Server is listening on port 3000");
+});
