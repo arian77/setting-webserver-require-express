91f1604e71bd4433524bc0a7f3fede782482232f
Arian <arianmarkus77@yahoo.com>
2017-11-08 22:20:10 +0700

    Create config and connecting database

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+// Configuring the database
+var dbConfig = require("./config/database.config.js");
+var mongoose = require("mongoose");
+
+mongoose.connect(dbConfig.url);
+
+mongoose.connection.on("error", function() {
+  console.log("Could not connect to the database. Exiting now...");
+  process.exit();
+});
+mongoose.connection.once("open", function() {
+  console.log("Successfully connected to the database");
+});
+
 // define a simple route
 app.get("/", function(req, res) {
   res.json({
